70e50643480c1bcb9ed27aa7e603717d9ab81879
iffat-siddick <k22031129@kcl.ac.uk>
2023-10-26 20:22:45 +0100

    set up foundation of answer

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    x = 1
+    total = 0
+    while (total<= number_of_primes):
+        if ():
+            list.append(x)
+            total += 1
+            
     return list
+
+numbers = primes(5)
+print(numbers)
